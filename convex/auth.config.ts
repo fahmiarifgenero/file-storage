3cb0f9951fbf441c8c6d0511efdf5327eb04bfca
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2024-03-24 13:43:05 +0700

    initial convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://light-boar-81.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
